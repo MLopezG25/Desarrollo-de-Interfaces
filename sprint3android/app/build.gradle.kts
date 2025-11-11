c1cf9adf3e4a492e8df9d945e397c3330a3dc446
Mateo López García <mlopezg24@fpcoruna.afundacion.org>
2025-11-11 23:12:43 +0100

    Ejercicio 4 correcto

--- sprint3android/app/build.gradle.kts
+++ sprint3android/app/build.gradle.kts
@@ -40,6 +40,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation libs.navigation.fragment
-            implementation libs.navigation.ui
 }
